7362b1512e1bbc934582250a40e8fc673000def2
unknown <vaibhav@A7.com>
2023-04-19 02:16:46 +0530

    second commit mailchimp

--- app.js
+++ app.js
@@ -19,7 +19,37 @@ app.post("/", function(req, res){
     var secondName = req.body.sName;
     var email = req.body.email;
 
-    console.log(firstName, secondName, email);
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed"
+            }
+        ]
+    };
+
+    var jsonData = JSON.stringify(data);
+
+
+    var options = {
+        url: "http://us21.api.mailchimp.com/3.0/lists/7e0dca60d6",
+        method:"post",
+        headers: {
+            "Authorization": "vaibhav 2335faed0949ce76bb9dcba9845e3a1a-us21"
+
+        },
+        body: jsonData
+
+    };
+
+
+    request(options , function(error, response, body){
+        if(error){
+            console.log(error);
+        }else{
+            console.log(response.statusCode);
+        }
+    });
 
 })
 
@@ -27,3 +57,15 @@ app.post("/", function(req, res){
 app.listen(3000, function(){
     console.log("server is on port 3000");
 });
+
+
+
+
+
+
+// API key
+// 2335faed0949ce76bb9dcba9845e3a1a-us21
+
+
+// list ID 
+// 7e0dca60d6
