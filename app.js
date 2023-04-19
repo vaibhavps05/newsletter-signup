0c453153e41b532d7e537ad65d755b1b0f00d93f
unknown <vaibhav@A7.com>
2023-04-19 17:36:55 +0530

    Final commit

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.post("/failure", function(req, res){
     res.redirect("/");
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("server is on port 3000");
 });
 
